f80f0bd9faaba11b5c66498519395f16ecddf624
shunya-toda <77889501+shunya-toda@users.noreply.github.com>
2021-02-26 22:44:15 +0900

    Initial commit

---
+++ Assets/CubeController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    private float speed = -12;
+
+    private float deadLine = -10;
+
+    bool contact = true;
+
+    public Bs bs;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(this.speed * Time.deltaTime, 0, 0);
+
+        if(transform.position.x<this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "CubeTag" || other.gameObject.tag == "groundTag")
+        {
+            if (contact)
+            {
+                this.bs.se();
+                contact = false;
+            }
+        }
+    }
+    public void SetUpbs(Bs bs)
+    {
+        this.bs = bs;
+    }
+
+
+}
